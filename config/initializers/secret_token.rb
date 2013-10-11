8041cb623c1a1b98597f9f2ca938635e901a80f8
marcelo <hemakesnoise@gmail.com>
2013-10-11 19:13:04 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emplefono::Application.config.secret_token = '2eacc4ca2375fb3100452c871e003bd55e508f50a76eec663471ce7ae3cfb6b6d7d96485a8bfff3469ce8b366c896644ce7445bc2f464f8ce95013a2e08e4d38'
